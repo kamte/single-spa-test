d7d33b8d0f2c25fe1680fa0ce06913f3b583bff1
Miguel Collado <mcollado@joor.com>
2020-07-08 15:47:41 +0200

    add button to trigger error boundary

--- single-spa/src/root.component.js
+++ single-spa/src/root.component.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@joor/core-components";
 
 export default function Root(props) {
+  const [value, setValue] = useState({ test: "is mounted" });
   return (
     <>
-      <section>{props.name} is mounted! yaaaaay</section>
+      <section>
+        {props.name} {value.test}
+      </section>
       <Button
         onClick={() => {
           alert("yay!");
         }}
       >
-          Alert me!
+        Alert me!
+      </Button>
+      <br />
+      <Button
+        onClick={() => {
+          setValue(undefined);
+        }}
+      >
+        Break stuff
       </Button>
     </>
   );
